63f70d11ba1ec4a7443a007b876025d6d30bb722
Nicholas Touikan <nicholas.touikan@gmail.com>
2017-12-11 11:04:41 -0500

    Typos!!!
    fixed some typos and crap code

--- mkvenv.sh
+++ mkvenv.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# This script should be run in tha base directory and sets up
+# This script should be run in the base directory and sets up
 # the virtual environment. This is archlinux-specific
 
 virtualenv -p /usr/bin/python2.7 .
+sh activateSource.sh
 pip2 install -r requirements.txt
